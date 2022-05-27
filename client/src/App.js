b245e97fe7d96ed2edd3544c5e63d16682634802
SalomonGuttmann <salomonguttmann@gmail.com>
2022-05-27 15:54:38 +0200

    add some changes in Dockerrun.aws.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-function App() {
+class App extends Component {
+	render() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Testind Deploy
-          </a>
+          <h1 className="App-title"> Fib Calculator version 2 </h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
@@ -29,6 +23,7 @@ function App() {
       </div>
     </Router>
   );
+ }		
 }
 
 export default App;
